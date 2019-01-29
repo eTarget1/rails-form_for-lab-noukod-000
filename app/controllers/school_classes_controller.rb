b28dea27c03dbc2ae63d57c6c0bc07a3f0b7b86d
clifford <clifford@noukod.com>
2019-01-29 17:11:14 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -5,33 +5,33 @@ class SchoolClassesController < ApplicationController
   end
 
   def new
-    @school_class = SchoolClass.new
-  end
+  @school_class=SchoolClass.new
+end
 
-  def create
-    @school_class = SchoolClass.new(school_classes_params(:title, :room_number))
-    @school_class.save
-    redirect_to school_classs_path(@school_class)
-  end
+def create
+  @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+  @school_class.save
+  redirect_to school_class_path(@school_class)
+end
 
-  def show
-    @school_class = SchoolClass.find(params[:id])
-  end
+def show
+  @school_class = SchoolClass.find(params[:id])
+end
 
-  def edit
-    @school_class = SchoolClass.find(params[:id])
+def edit
+  @school_class = SchoolClass.find(params[:id])
+end
 
-  end
+def update
+  @school_class = SchoolClass.find(params[:id])
+  @school_class.update(school_class_params(:title, :room_number))
+  redirect_to school_class_path(@school_class)
+end
 
-  def update
-    @school_class = SchoolClass.find(params[:id])
-    @school_class.update(school_class_params(:title, :room_number))
-    redirect_to school_class_path(@school_class)
-  end
+private
+def school_class_params(*args)
+params.require(:school_class).permit(*args)
+end
 
-  private
-  def school_class_params(*args)
-    params.require(:school_class).permit(*args)
-  end
 
 end
